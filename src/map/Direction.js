36386e89e2d8aaab8abf60a2fac632972d194ea0
Bovard Tiberi <bovard.tiberi@gmail.com>
2014-03-09 19:00:57 -0600

    Added the location

--- src/map/Direction.js
+++ src/map/Direction.js
@@ -7,6 +7,7 @@ Direction.NORTH = new Direction(0, 1);
 Direction.EAST = new Direction(0, 1);
 Direction.WEST = new Direction(0, -1);
 Direction.SOUTH = new Direction(-1, 0);
+Direction.NONE = new Direction(0, 0);
 
 Direction.prototype = {
     rotateRight: function(dir) {
---
+++ src/map/Location.js
@@ -0,0 +1,45 @@
+var Direction = require('./Direction');
+
+function Location(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Location.prototype = {
+    isEqualTo: function(location) {
+        return this.x && this.y && this.x === location.x && this.y === location.y;
+    },
+    add: function(direction, times) {
+        if (!times) {
+            times = 1;
+        }
+        this.x += times * direction.x;
+        this.y += times * direction.y;
+    },
+    distanceSquaredTo: function(location) {
+        return Math.pow(this.x - location.x, 2) + Math.pow(this.y - location.y, 2);
+    },
+    directionTo: function(location) {
+        var x = location.x - this.x;
+        var y = location.y - this.y;
+        if (x === 0 && y === 0) {
+            return Direction.NONE;
+        }
+        if (Math.abs(x) > Math.abs(y)) {
+            if (x > 0) {
+                return Direction.EAST;
+            } else {
+                return Direction.WEST;
+            }
+        } else {
+            if (y > 0) {
+                return Direction.NORTH;
+            } else {
+                return Direction.SOUTH;
+            }
+        }
+
+    }
+};
+
+module.exports = Location;
